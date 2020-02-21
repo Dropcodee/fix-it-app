197e2b47e326163a9f5d2cf498476a7d57f87081
forbes <afolabi2245@gmail.com>
2020-02-22 00:10:25 +0100

    Signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
-import 'package:flutter_app/login.dart';
 import 'package:flutter/material.dart';
+import 'signup/signup.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fix It',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: FormScreen(),
+      title: 'Flutter Demo',
+
+      home: Scaffold(
+        body: Center(
+          child: Signup()
+        )
+      )
     );
   }
 }
